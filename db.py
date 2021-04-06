404422d70697663205b5f2846446cc8b0a5f7c7b
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-04-06 10:47:43 +0300

    инициализация БД

---
+++ db.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+PWD = 'password'
+USR= 'root'
+
+SQLALCHEMY_DATABASE_URL = 'mysql://{}:{}@localhost:3306/base'.format(USR, PWD)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
+Base = declarative_base()
+#функция, которая создает базовый класс
+# для декларативных определений классов
