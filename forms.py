df0fd0e449cd25e0e3b4a559884fdd11ffc346d4
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-04-06 15:18:41 +0300

    описание класса форм

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+
+class AuthForm(FlaskForm):
+    #user_id = StringField('user_id', validators=[DataRequired()])
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
+    email = StringField('email', validators=[Email()])
+    login = StringField('login', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    
