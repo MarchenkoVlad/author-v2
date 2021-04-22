e453b946220d336514d8eefedd9a82efc0429c78
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-04-22 18:47:45 +0300

    добавлен маршрут для 401

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template
 from db import engine
 from crypt import Bcrypt
 from routers import wrap
+from flask_login import LoginManager
 import db
+from models import User
 
 db.Base.metadata.create_all(bind=engine)
 
@@ -11,6 +13,14 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_ECHO'] =True
+
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = "wrap.login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 app.register_blueprint(wrap)
 
